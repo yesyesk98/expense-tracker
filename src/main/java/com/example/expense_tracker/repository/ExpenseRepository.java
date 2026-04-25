f6ab97d91579851de87ae147366cd43618cd82d1
sri krishna simhadri <srikrishnasimhadri@sris-MBP.attlocal.net>
2026-04-24 22:27:22 -0500

    Update expense repository

---
+++ src/main/java/com/example/expense_tracker/repository/ExpenseRepository.java
@@ -0,0 +1,9 @@
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.expense_tracker.model.Expense;
+
+@Repository
+public interface ExpenseRepository extends JpaRepository<Expense, Long> {
+    
+}
